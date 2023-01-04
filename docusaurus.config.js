f07ce5ba08beb8064b9e7746a27a6bf145346aa4
mwkirk <micah@kirkpatrickcore.com>
2023-01-03 20:52:23 -0500

    config updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,17 +8,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'The Open-Source Guide to the Theory of Probability and Machine Learning',
   tagline: 'Computational Applications With Code',
-  url: 'https://www.kirkpatrickcore.com/topaml',
-  baseUrl: '/',
+  url: 'https://www.kirkpatrickcore.com/',
+  baseUrl: '/topaml/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/charolee_chanard.ico',
+  staticDirectories: ['static'],
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'KirkpatrickCORE', // Usually your GitHub org/user name.
   projectName: 'topaml', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'prod',
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
